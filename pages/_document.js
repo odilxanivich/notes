b82602689ffc0a044ef4d832cfa439dfa0da9573
odilxanivich <odilxanivich@gmail.com>
2026-05-28 10:16:58 +0900

    v3

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,18 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+
+        {/* PWA & theme */}
         <meta name="theme-color" content="#1c1c1e" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        {/* ✅ THE ZOOM FIX */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
+
+        {/* Link to manifest */}
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
         <Main />
